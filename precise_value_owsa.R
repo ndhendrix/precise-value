e7a15f7ee1f540a5f5a5d01e16c32255316a1402
Nathaniel D Hendrix <ndhendrix@gmail.com>
2020-07-20 11:06:53 -0700

    Add base case values file
    Removed setting base case defaults in each file and instead put all base case values in a single file which is then accessed by other files.

--- precise_value_owsa.R
+++ precise_value_owsa.R
@@ -1,67 +1,8 @@
-###
-### Set base case parameters
-###
-
-# Set demographic parameters
-n <- 32000  #population size
-
-# racial demographics from overall US demographics
-p_a <- 0.06 #percentage of population Asian
-p_b <- 0.13 #percentage of population Black
-p_l <- 0.18 #percentage of population Latinx
-p_w <- 0.60 #percentage of population White
-p_o <- 0.03 #percentage of population other race
-
-# calculated from race-specific prevalence of CYP2C19 variants
-p_clo_a <- 0.1298 #prevalence of clopidogrel variant (poor metabolizers) among Asian (E. Asian) patients
-p_clo_b <- 0.0405 #prevalence of clopidogrel variant (poor metabolizers) among Black (African American) patients
-p_clo_l <- 0.0114 #prevalence of clopidogrel variant (poor metabolizers) among Latinx (Latino) patients
-p_clo_w <- 0.2722 #prevalence of clopidogrel variant (poor metabolizers) among White (European) patients
-p_clo_o <- 0.1061 #prevalence of clopidogrel variant (poor metabolizers) among other race (average of all races) patients
-
-# calculated from race-specific prevalence of CYP2C9 variants
-p_sim_a <- 0.1575 #prevalence of simvastatin variant (poor or medium metabolizers) among Asian (E. Asian) patients
-p_sim_b <- 0.2413 #prevalence of simvastatin variant (poor or medium metabolizers) among Black (African American) patients
-p_sim_l <- 0.2542 #prevalence of simvastatin variant (poor or medium metabolizers) among Latinx (Latino) patients
-p_sim_w <- 0.3708 #prevalence of simvastatin variant (poor or medium metabolizers) among White (European) patients
-p_sim_o <- 0.2599 #prevalence of simvastatin variant (poor or medium metabolizers) among other (average of all races) race patients
-
-# assumes that CYP2C9 and VKORC1 are independent 
-p_war_a <- 0.9006 #prevalence of warfarin variant among Asian (E. Asian) patients
-p_war_b <- 0.3392 #prevalence of warfarin variant among Black (African American) patients
-p_war_l <- 0.6005 #prevalence of warfarin variant among Latinx (Americas) patients
-p_war_w <- 0.6303 #prevalence of warfarin variant among White (Caucasian) patients
-p_war_o <- 0.5743 #prevalence of warfarin variant among other (average of all races) race patients
-
-discount <- 0.03
 
 # probability of plan modification based on alert / no alert
-p_change_alert_default <- 0.9
-p_change_alert <- p_change_alert_default
-p_change_no_alert <- 0.1
-
-# qalys and costs of changing pgx
-qaly_change_clo <- 0.05
-cost_change_clo <- 1265
-qaly_change_sim <- 0  # simvastatin no longer included
-cost_change_sim <- 0  # as of 6/22
-qaly_change_war <- 0.01
-cost_change_war <- -50
-
-# time horizon (years)
-t_horizon <- 20
-
-# costs associated with alert
-start_up_cost_default <- 4000
-maint_cost_default <- 100
-start_up_cost <- start_up_cost_default
-maint_cost <- maint_cost_default
-
-# testing and medication use patterns
-screen_dur <- 5 # years of screening
-p_new_rx <- 0.005 # annual probability of a new rx for one of the included drugs
-test_rate <- 0.1 # annual probability of testing for patients in the selected age range
-start_age <- 55 # age at start of screening
+p_change_alert_default <- p_change_alert
+start_up_cost_default <- start_up_cost
+maint_cost_default <- maint_cost
 screen_dur_current <- screen_dur
 p_new_rx_current <- p_new_rx
 test_rate_current <- test_rate
@@ -72,6 +13,7 @@ start_age_current <- start_age
 ###
 
 source("D:/ndhen/Dropbox/School/RA/2019/Cost-effectiveness model/precise-value/precise_value_r_model.R") 
+source("D:/ndhen/Dropbox/School/RA/2019/Cost-effectiveness model/precise-value/precise_value_base_values.R") 
 total <- precise_value()[[2]] 
 
 base_icer <-  round((sum(total$alert_cost) - sum(total$no_alert_cost))/(sum(total$alert_qaly) - sum(total$no_alert_qaly)),2)
--- precise_value_psa.R
+++ precise_value_psa.R
@@ -5,42 +5,11 @@
 # set number of runs for psa
 n_runs <- 100
 
-# Set demographic parameters
-n <- 32000  #population size
-
-# time horizon (years)
-t_horizon <- 20
-
-# discount
-discount <- 0.03
-
 # default testing and medication use patterns
-screen_dur_default <- 5 # years of screening
-p_new_rx_default <- 0.005 # annual probability of a new rx for one of the included drugs
-test_rate_default <- 0.1 # annual probability of testing for patients in the selected age range
-start_age_default <- 55 # age at start of screening
-
-# calculated from race-specific prevalence of CYP2C19 variants
-p_clo_a <- 0.1298 #prevalence of clopidogrel variant (poor metabolizers) among Asian (E. Asian) patients
-p_clo_b <- 0.0405 #prevalence of clopidogrel variant (poor metabolizers) among Black (African American) patients
-p_clo_l <- 0.0114 #prevalence of clopidogrel variant (poor metabolizers) among Latinx (Latino) patients
-p_clo_w <- 0.2722 #prevalence of clopidogrel variant (poor metabolizers) among White (European) patients
-p_clo_o <- 0.1061 #prevalence of clopidogrel variant (poor metabolizers) among other race (average of all races) patients
-
-# calculated from race-specific prevalence of CYP2C9 variants
-p_sim_a <- 0.1575 #prevalence of simvastatin variant (poor or medium metabolizers) among Asian (E. Asian) patients
-p_sim_b <- 0.2413 #prevalence of simvastatin variant (poor or medium metabolizers) among Black (African American) patients
-p_sim_l <- 0.2542 #prevalence of simvastatin variant (poor or medium metabolizers) among Latinx (Latino) patients
-p_sim_w <- 0.3708 #prevalence of simvastatin variant (poor or medium metabolizers) among White (European) patients
-p_sim_o <- 0.2599 #prevalence of simvastatin variant (poor or medium metabolizers) among other (average of all races) race patients
-
-# assumes that CYP2C9 and VKORC1 are independent 
-p_war_a <- 0.9006 #prevalence of warfarin variant among Asian (E. Asian) patients
-p_war_b <- 0.3392 #prevalence of warfarin variant among Black (African American) patients
-p_war_l <- 0.6005 #prevalence of warfarin variant among Latinx (Americas) patients
-p_war_w <- 0.6303 #prevalence of warfarin variant among White (Caucasian) patients
-p_war_o <- 0.5743 #prevalence of warfarin variant among other (average of all races) race patients
-
+screen_dur_default <- screen_dur # years of screening
+p_new_rx_default <- p_new_rx # annual probability of a new rx for one of the included drugs
+test_rate_default <- test_rate # annual probability of testing for patients in the selected age range
+start_age_default <- start_age # age at start of screening
 
 ###
 ### load model 
@@ -58,6 +27,13 @@ output <- data.frame(run = seq(1,n_runs),
                      costs = rep(0,n_runs),
                      qalys = rep(0, n_runs))
 
+# set default demographic values
+p_a_default <- p_a
+p_b_default <- p_b
+p_l_default <- p_l
+p_w_default <- p_w
+p_o_default <- p_o
+
 # run psa
 for(i in 1:n_runs) {
   
@@ -65,7 +41,11 @@ for(i in 1:n_runs) {
   plan_demographics <- prop.table(table(sample(c("a", "b", "l", "w", "o"),
                                                n,
                                                replace = TRUE,
-                                               prob = c(0.06, 0.13, 0.18, 0.60, 0.03))))
+                                               prob = c(p_a_default, 
+                                                        p_b_default, 
+                                                        p_l_default, 
+                                                        p_w_default, 
+                                                        p_o_default))))
   p_a <- plan_demographics["a"] #percentage of population Asian
   p_b <- plan_demographics["b"] #percentage of population Black
   p_l <- plan_demographics["l"] #percentage of population Latinx
@@ -93,8 +73,6 @@ for(i in 1:n_runs) {
   p_new_rx_current <- runif(1, 0.0025, 0.0075) # annual probability of a new rx for one of the included drugs
   test_rate_current <- runif(1, 0.075, 0.125) # annual probability of testing for patients in the selected age range
   start_age_current <- round(runif(1, 50, 60),0) # age at start of screening
-  
-  print(paste0(screen_dur_current, " ", test_rate_current, " ", start_age_current))
   
   # recreate datasets with test and treat info
   make_test_pattern()
