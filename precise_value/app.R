d20bc497ec377c2d819519e8859888d1d55933b5
Patrick Mathias <pcmathias@gmail.com>
2021-05-04 17:14:44 -0700

    add markdown to explanation page

--- precise_value/app.R
+++ precise_value/app.R
@@ -1,6 +1,7 @@
 
 library(shiny)
 library(shinydashboard)
+library(knitr)
 library(here)
 library(tidyverse)
 library(DT)
@@ -421,35 +422,38 @@ ui <- dashboardPage(
                     )
                     ),
             tabItem(tabName = "info",
-                    tags$ul(
-                        h2("Primer on Economic Evaluation"),
-                        
-                        h3("Economic evaluation"),
-                        tags$li("Economic evaluation uses a mathematical model to compare two or more alternative courses of action in terms of both their costs and effectiveness. "),
-                        
-                        h3("Value"),
-                        tags$li("Trade-off between costs and effectiveness (health outcomes) when comparing multiple health interventions"),
-                        
-                        h3("Measures of effectiveness (health outcomes)"),
-                        tags$li("Quality-adjusted life years (QALYs) that combines (1) quantity of life (survival), and (2)quality of life."),
-                        tags$li("The number of adverse events."),
-                        tags$li("Others possible."),
-                        
-                        h3("Results of economic evaluation"),
-                        tags$li("Costs"),
-                        tags$li("Total costs of each intervention"),
-                        h5("Incremental costs comparing two interventions, i.e. Cost of intervetnion A - Cost of intervention B"),
-                        h4("Effectiveness"),
-                        h5("Total QALYs of each intervention"),
-                        h5("Incremental effectiveness comparing two interventions, i.e. QALYs of intervention A - QALYs of intervention B"),
-                        h4("Incremental costs and effectiveness ratio (ICER)=Incremental Costs / Incremental QALYs"),
-                        
-                        h3("Decision made after economic evaluation"),
-                        h4("What is your willingness to pay (WTP) for one unit increase in effectiveness?"),
-                        h5("Note that US WTP is between $50,000 and $100,000 per QALY gained"),
-                        h4("If your estimated ICER is below your chosen WTP, consider implementing the tested intervention"),
-                        h4("If your estimated ICER is above your chosen WTP, consider implementing the alternative intervention")
+                    fluidPage(
+                        includeMarkdown(here("R", "explanation_markdown.Rmd"))
                     )
+                    # tags$ul(
+                    #     h2("Primer on Economic Evaluation"),
+                    #     
+                    #     h3("Economic evaluation"),
+                    #     tags$li("Economic evaluation uses a mathematical model to compare two or more alternative courses of action in terms of both their costs and effectiveness. "),
+                    #     
+                    #     h3("Value"),
+                    #     tags$li("Trade-off between costs and effectiveness (health outcomes) when comparing multiple health interventions"),
+                    #     
+                    #     h3("Measures of effectiveness (health outcomes)"),
+                    #     tags$li("Quality-adjusted life years (QALYs) that combines (1) quantity of life (survival), and (2)quality of life."),
+                    #     tags$li("The number of adverse events."),
+                    #     tags$li("Others possible."),
+                    #     
+                    #     h3("Results of economic evaluation"),
+                    #     tags$li("Costs"),
+                    #     tags$li("Total costs of each intervention"),
+                    #     h5("Incremental costs comparing two interventions, i.e. Cost of intervetnion A - Cost of intervention B"),
+                    #     h4("Effectiveness"),
+                    #     h5("Total QALYs of each intervention"),
+                    #     h5("Incremental effectiveness comparing two interventions, i.e. QALYs of intervention A - QALYs of intervention B"),
+                    #     h4("Incremental costs and effectiveness ratio (ICER)=Incremental Costs / Incremental QALYs"),
+                    #     
+                    #     h3("Decision made after economic evaluation"),
+                    #     h4("What is your willingness to pay (WTP) for one unit increase in effectiveness?"),
+                    #     h5("Note that US WTP is between $50,000 and $100,000 per QALY gained"),
+                    #     h4("If your estimated ICER is below your chosen WTP, consider implementing the tested intervention"),
+                    #     h4("If your estimated ICER is above your chosen WTP, consider implementing the alternative intervention")
+                    # )
             ),
             tabItem(tabName = "interp",
                     h2(
@@ -507,7 +511,9 @@ server <- function(input, output, session) {
                                       decision makers in Learning Health Systems, who are 
                                       considering using PGx to improve the quality of patient 
                                       care.")
-   
+    # output$explanation_markdown <- renderUI({
+    #     HTML(markdown::markdownToHTML(knit(here("R", "explanation_markdown.Rmd"), quiet = TRUE)))
+    # })
     output$n_alerts <- renderValueBox({
         valueBox(
             round(data()$n_alerts, 0), "Total number of alerts",
